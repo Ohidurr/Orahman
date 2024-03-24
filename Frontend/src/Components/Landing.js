b32e0f3f51d6717cf3096aa9fa423ec5eea7e7a2
Ohidurr <OhidurRahman96@Gmail.Com>
2024-03-24 16:18:13 -0400

    add waving function on emoji

--- Frontend/src/Components/Landing.js
+++ Frontend/src/Components/Landing.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Landing.css'; // Import the specific CSS file for Landing
 
 function Landing() {
+  const [isWaving, setIsWaving] = useState(false);
+
+  const toggleWave = () => {
+    setIsWaving(true);
+    setTimeout(() => setIsWaving(false), 1000); // Adjust timing as needed
+  };
+
   return (
-    <section id="landing">
-      <h1>👋</h1>
+    <section id="landing" onClick={toggleWave}>
+      <h1>
+        <span className={isWaving ? 'wave' : ''}>👋</span>{" "}
+      </h1>
       <h1>Ohidur Rahman</h1>
       <p>Welcome.</p>
     </section>
@@ -12,3 +21,4 @@ function Landing() {
 }
 
 export default Landing;
+;
