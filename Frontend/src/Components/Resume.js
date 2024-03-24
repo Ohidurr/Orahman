ed20370ab3a57a2bb56b495c2a2cdc42c91d501e
Ohidurr <OhidurRahman96@Gmail.Com>
2024-03-23 22:21:09 -0400

    place holder for resume css viewing

---
+++ Frontend/src/Components/Resume.js
@@ -0,0 +1,13 @@
+// // Components/Resume.js
+// import React from 'react';
+
+// const Resume = () => {
+//   return (
+//     <div id="resumeSection">
+//       {/* Embed your resume or display information here */}
+//       <p>Resume content goes here.</p>
+//     </div>
+//   );
+// }
+
+// export default Resume;
