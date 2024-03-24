20e99b6a3b6d6654e25a848f8a3413f47d0d493a
Ohidurr <OhidurRahman96@Gmail.Com>
2024-03-24 12:44:09 -0400

    add fontawesome, svg social media links and mobile response

--- Frontend/src/Components/Contact.js
+++ Frontend/src/Components/Contact.js
@@ -1,10 +1,26 @@
 import React from 'react';
+// Importing Font Awesome components and specific icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Contact() {
   return (
     <section id="contact">
       <h2>Contact</h2>
-      {/* Contact form and social links go here */}
+      <div className="social-links">
+        {/* Updated Twitter (X) icon using an <img> tag */}
+        <a href="https://x.com/_Ohidur" target="_blank" rel="noopener noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/X_logo_2023_original.svg" alt="X Logo" style={{ width: '32px', height: '32px' }} />
+        </a>
+        {/* LinkedIn */}
+        <a href="https://linkedin.com/in/Ohidur-Rahman" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+        </a>
+        {/* GitHub */}
+        <a href="https://github.com/Ohidurr" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+      </div>
     </section>
   );
 }
